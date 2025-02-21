3892a9cb5cd1b0ebbe3eaed55ca8509c79ff94a8
GrimReaper <38589063+Reaper-Bot@users.noreply.github.com>
2025-02-22 06:57:30 +0700

    Update models.py
    update user models

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)  # Store hashed password
-    role = db.Column(db.String(20), nullable=False, default="user")
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), nullable=False, default="user")  # Can be 'admin' or 'user'
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
